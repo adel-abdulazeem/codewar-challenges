e7757d0a6a51749f7809d9072d86301ca3b7a0ef
Adel Abdulazeem <adelnasr029@gmail.com>
2025-03-06 22:31:57 +0200

    sumSquareEvenRootOdd

--- sortMaxMin.js
+++ sortMaxMin.js
@@ -1,10 +1,15 @@
 function solve(arr){
     let sortedArr = arr.sort((a,b) => a - b)
     let res = []
-    while( sortedArr.length){
-      res.push(sortedArr.pop(), sortedArr.shift())
-    }
-    return res[res.length - 1]? res : res.slice(0, -1)
+    // while( sortedArr.length){
+    //   res.push(sortedArr.pop(), sortedArr.shift())
+    // }
+    // // return res[res.length - 1]? res : res.slice(0, -1)
+    //     console.log(sortedArr)
+    //     console.log(sortedArr.pop())
+    //     console.log(sortedArr.shift())
+        res.push(sortedArr.pop(), sortedArr.shift())
+        return res
     };
     
-    console.log(solve([1,6,9,4,3,7,8,2]), '[9,1,8,2,7,3,6,4]')
+  console.log(solve([1,6,9,4,3,7,8,2]))
