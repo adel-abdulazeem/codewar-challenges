97bff48b7a22b1b40cbe2a5957e8eaf1cd97dfa4
Adel Abdulazeem <adelnasr029@gmail.com>
2025-03-21 13:50:29 +0200

    new challenge

--- js/main.js
+++ js/main.js
@@ -1,2 +1,28 @@
 //abcdefghijklmnopqrstuvwxyz0123456789
 
+// func takes a num => return a str or object based on the conditional.
+// param, always num, valid, whole num
+// object or only one key value pairs, or str
+// 
+
+
+function iTri(s){
+    //triathalon consists of 2.4 swim, 112 bike, 26.2 run
+    //its gonna be a long day!
+    // created var tht hold total distance val
+    // then conditional to return values based on the param and triatholan structure
+    // calculating remaining dis by subtracting the param from the total just in case if param isnot 0 or 140.6
+    const total = 2.4 + 112 + 26.2
+    if(s === 0) return 'Starting Line... Good Luck!'
+    else if(s <= 2.4) return {"Swim": `${Number(total - s).toFixed(2)} to go!`}
+    else if(s <= 114.4) return {"Bike": `${Number(total - s).toFixed(2)} to go!`}
+    else if(s <= 130.6) return {"Run": `${Number(total - s).toFixed(2)} to go!`}
+    else if (s < total) return {'Run': 'Nearly there!'}
+    return "You're done! Stop running!"
+  }
+
+    console.log(iTri(36)) // {'Bike':'104.60 to go!'}
+    console.log(iTri(103.5),"{'Bike':'37.10 to go!'}")
+    console.log((iTri(0),'Starting Line... Good Luck!'))
+    console.log(iTri(2),"{'Swim':'138.60 to go!'}")
+
