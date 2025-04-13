fe0661ff261a097492a099a740301da18a6a13db
Adel Abdulazeem <adelnasr029@gmail.com>
2025-04-13 09:57:29 +0200

    new challenge

--- js/main.js
+++ js/main.js
@@ -1,4 +1,13 @@
 //abcdefghijklmnopqrstuvwxyz0123456789
+function Person (name) {
+return {
+  name: name,
+  greet: function() {
+    return `Hello, my name is ${name}`;
+  }
+}
+}
 
+const person1 =  Person("Alice");
 
-
+console.log(person1.greet()); // Output: Hello, my name is Alice
