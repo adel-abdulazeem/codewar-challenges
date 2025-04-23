a42ecc262103b2c9f5ace390d92de675b3d64dad
Adel Abdulazeem <adelnasr029@gmail.com>
2025-04-23 23:36:56 +0200

    new challenge

--- js/main.js
+++ js/main.js
@@ -1,3 +1,12 @@
 // write a func that takes fn as arg and it return a num
 
-console.log(parseInt("c", 16)) // 44
+function Person(name, age) {
+  this.name = name;
+  this.age = age;
+  this.sayHello = function() {
+    console.log("Hello, my name is " + this.name + " and I am " + this.age + " years old.");
+  };
+
+} 
+var person = Person()
+console.log(person("John", 30))
