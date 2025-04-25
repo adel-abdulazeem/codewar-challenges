8cf8f88cb46ec19d2888a5c5d9356c9b6861c34a
Adel Abdulazeem <adelnasr029@gmail.com>
2025-04-26 02:06:20 +0300

    new challenge

---
+++ greetByOrNUll.js
@@ -0,0 +1,10 @@
+// write a func tht takes name and return text or null
+// name is str || empty || null
+// str || nul
+function greet(name) {
+    // conditional
+    if(name){
+        return `hello ${name}!`
+    }
+    return null
+}
