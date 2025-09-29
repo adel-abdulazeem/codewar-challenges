d5b19020fdb739966bf1b0b0d2e21359daa328bb
Adel <adel_abdulazeem@outlook.com>
2025-09-29 23:45:38 +0300

    Counter

---
+++ Counter.js
@@ -0,0 +1,12 @@
+
+// make a constructor function with two methods (and only these two methods) to return and increment a counter, but the counter should not be directly accessible from outside the function.
+
+
+
+function Counter() {
+
+    let count =
+        this.check = () => count
+    this.increment = () => count++
+
+};
