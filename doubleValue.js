050d444cc63bd7cc69246e1f4c4533e33a5696e1
Adel <adel_abdulazeem@outlook.com>
2025-10-02 03:22:25 +0300

    static var& method

---
+++ doubleValue.js
@@ -0,0 +1,12 @@
+class Class {
+    static number = 1
+
+    static getNumber() {
+        const currentVal = Class.number
+        Class.number *= 2
+        return currentVal
+    }
+}
+
+console.log(Class.getNumber())
+console.log(Class.getNumber())
